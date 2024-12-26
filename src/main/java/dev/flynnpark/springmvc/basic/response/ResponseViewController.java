bb0765929cd6dd6a19f5aabceb7b522e6fb00f94
Flynn <wphestiraid@gmail.com>
2024-12-26 09:06:24 +0900

    스프링 MVC: 기본 기능 - HTTP 응답: 정적 리소스, 뷰 템플릿

---
+++ src/main/java/dev/flynnpark/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,14 @@
+package dev.flynnpark.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data", "hello!");
+        return mav;
+    }
+}
